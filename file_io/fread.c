d084ee1ba76e23bfd260af72ba4cee47f4eb2a70
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-06-23 13:27:30 +0530

    organize

--- file_io/fread.c
+++ file_io/fread.c
@@ -11,10 +11,11 @@ int main()
     count = fread(&buffer, sizeof(char), 10, stream);
     printf("Data read from the file: %s \n", buffer);
     printf("Number of elements read: %d \n", count);
+
     count = fread(&buffer, sizeof(char), 10, stream);
     printf("Data read from the file: %s \n", buffer);
     printf("Number of elements read: %d \n", count);
-
     fclose(stream);
+
     return 0;
 }
--- test_itos.c
+++
@@ -1,9 +0,0 @@
-// another_file.c
-#include "itos.h"
-#include <stdio.h>
-
-int main()
-{
-    printf("%c\n", itos[4]); // Access itos array
-    return 0;
-}
