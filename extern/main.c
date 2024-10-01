ba930adee62801eec7a5efd8a40e909e64a471f5
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-10-02 01:43:46 +0530

    -

---
+++ extern/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "t.h"
+
+
+int main(void){
+  int result = add(1, 2);
+  printf("Result = %d \n", result);
+  return 0;
+}
---
+++ extern/t.h
@@ -0,0 +1,10 @@
+#ifndef T_H
+#define T_H
+
+#ifndef __cplusplus
+extern "C" 
+#endif
+
+int add(int, int);
+
+#endif
