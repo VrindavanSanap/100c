11511e4a06cb97f5e22d5a009d28feee47442647
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-02-02 00:31:33 +0530

    -

--- xor_encryption/encrypt.c
+++ xor_encryption/encrypt.c
@@ -13,25 +13,27 @@ void print_str(const char* s){
 }
 
 
-char* encrypt(const char* data,const char* key){
-  int data_size = (int)strlen(data);  
+char* encrypt(const char* data,const char* key, int data_size){
   int key_size = (int)strlen(key);  
   char* enc= (char*)malloc((data_size + 1) * sizeof(char));
+  printf("%d \n", data_size);
   for (int i = 0; i < data_size; i++){
     char data_i = data[i] ;
     char key_i = key[i%key_size];
     char xor_i = data_i ^ key_i;
-    printf("%c, %c, %d \n", data_i, key_i, xor_i);
+    printf("%x," , xor_i);
     enc[i] = xor_i;
   } 
+  printf("\n");
   enc[data_size] = '\0';
   return enc;
 }
 int main(void){
-  const char* data = "qwertyuiopasdfghjklzxcvbnm";
+
+  const char* data = "Blaze your glory";
   const char* key = "Doge";
-  const char* enc = encrypt(data, key);
-  const char* enc_enc = encrypt(enc, key);
+  const char* enc = encrypt(data, key, strlen(data));
+  const char* enc_enc = encrypt(enc, key, strlen(data));
   printf("%s", enc_enc);
   return 0;
 }
