64c7b714d4a5c18cfe221899eae3455996e10544
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-06-14 11:55:08 +0530

    cleanup and create makefiles

--- advanced_c/luts/enum.c
+++ advanced_c/luts/enum.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-
-int main(){
-  enum LEVEL{
-    A =20, B, C=11, D, E,
+int main() {
+  enum LEVEL {
+    A = 20,
+    B,
+    C = 11,
+    D,
+    E,
   };
- 
- printf("%d %d %d %d %d\n", A, B, C, D, E);
+
+  printf("%d %d %d %d %d\n", A, B, C, D, E);
 
   return 0;
 }
--- basics/bool.c
+++ basics/bool.c
@@ -3,8 +3,8 @@
     true and false in c are just 0 and 1
 */
 
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
   // this is exactly same as int x = 1
