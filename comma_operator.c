5d3536e77ede9ed2a22b4fb848029a4cea4518b9
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-08 00:11:49 +0530

    added datastrcutre stuff

---
+++ comma_operator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 69;
+  int b = 420;
+  /*
+  The comma operator in C evaluates the first operand, discards the result,
+  then evaluates the second operand and returns its value.
+  */
+  int c = (a, b);
+  printf("c = %d\n", c);
+
+  /*
+  The comma operator has the lowest precedence of all C operators.
+  In this case, 'a' is assigned to 'd' because the assignment operator has higher precedence.
+  The expression 'b' is evaluated, but its result is discarded.
+  */
+  int d = a, b;
+  printf("d = %d\n", d);
+
+  /*
+  For example, in the following macro, the return value of assert() is discarded.
+  The macro then returns the value of _Py_CAST(PyListObject *, (op)).
+  #define _PyList_CAST(op) \
+    (assert(PyList_Check(op)), _Py_CAST(PyListObject *, (op)))
+
+  */
+
+  return 0;
+}
---
+++ data_structures/dynamic_array/readme.md
@@ -0,0 +1,8 @@
+# Dynamic Array
+
+A [dynamic array](https://grokipedia.com/page/Dynamic_array) is a data structure that specifies *how* operations are performed.
+
+## Implementations
+
+- [CPython listobject.c](https://github.com/python/cpython/blob/main/Objects/listobject.c)
+- [OpenJDK ArrayList.java](https://github.com/openjdk/jdk/blob/master/src/java.base/share/classes/java/util/ArrayList.java)
