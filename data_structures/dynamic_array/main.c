a202206f8ae2257795b42cd55e0a13b3addee422
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-09 20:03:57 +0530

    Added demo

--- data_structures/dynamic_array/main.c
+++ data_structures/dynamic_array/main.c
@@ -8,5 +8,33 @@ typedef struct {
 
 int main() {
   dynamic_array *da = da_build(sizeof(point));
+  size_t size;
+
+  size = da_get_size(da);
+  printf("size = %zu \n", size);
+
+  for (int i = 0; i < 10; i++) {
+    point p = {i, i + i};
+    da_insert_last(da, &p);
+  }
+  size = da_get_size(da);
+  printf("size = %zu \n", size);
+
+  for (int i = 0; i < 10; i++) {
+    point p;
+    da_get_at(da, i, &p);
+    printf("%d %d \n", p.x, p.y);
+
+  }
+
+  for (int i = 0; i < 10; i++) {
+    point p;
+    da_delete_last(da);
+  }
+  size = da_get_size(da);
+  printf("size = %zu \n", size);
+
+
+
   return 0;
 }
