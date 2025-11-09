959e0a678cc914bedd919b8a8d3e10037fb7e359
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-09 21:40:13 +0530

    added boilerplate configuration for set_interface(dyn-arr implementation)

---
+++ data_structures/dynamic_array/set_interface/set_interface.h
@@ -0,0 +1,17 @@
+/*
+  Set interface implementation using dynamic-array datastruture
+  Operations
+    All these operatoins have time complexity of O(n)
+    Container
+      build(A) :build a set given iterable A
+    Static 
+      find(k)
+    Dynamic
+      insert(k)
+      delete(k)
+    Order 
+      find_min()
+      find_max()
+      find_prev(k)
+      find_next(k)
+*/
