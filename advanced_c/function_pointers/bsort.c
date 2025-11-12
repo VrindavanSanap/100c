f23ee7a03a86f69c30f6d628b7232625c43c1113
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-12 17:05:45 +0530

    -

--- advanced_c/function_pointers/bsort.c
+++ advanced_c/function_pointers/bsort.c
@@ -3,20 +3,20 @@
 #include <string.h>
 #include <utils/utils.h>
 
-int compare_ints(const void* p, const void* q) {
-  int num1 = *(const int*)p;
-  int num2 = *(const int*)q;
+int compare_ints(const void *p, const void *q) {
+  int num1 = *(const int *)p;
+  int num2 = *(const int *)q;
   return num1 - num2;
 }
-int compare_strs(const void* p, const void* q) {
-  char* str1 = *(char**)p;
-  char* str2 = *(char**)q;
+int compare_strs(const void *p, const void *q) {
+  char *str1 = *(char **)p;
+  char *str2 = *(char **)q;
   return strcmp(str1, str2);
 }
 
 // Swap function to exchange two elements
 // We use unsigned char to handle byte-wise copying safely for any data type
-void swap(void* a, void* b, size_t size) {
+void swap(void *a, void *b, size_t size) {
   unsigned char *p = a, *q = b, temp;
   for (size_t i = 0; i < size; i++) {
     temp = p[i];
@@ -25,8 +25,8 @@ void swap(void* a, void* b, size_t size) {
   }
 }
 
-void bsort(void* arr, int num, int size,
-           int (*compare_elm)(const void*, const void*)) {
+void bsort(void *arr, int num, int size,
+           int (*compare_elm)(const void *, const void *)) {
   for (int i = 0; i < num; i++) {
     for (int j = 0; j < num - 1 - i; j++) {
       if (compare_elm(arr + j * size, (arr + (j + 1) * size)) > 0) {
@@ -37,7 +37,7 @@ void bsort(void* arr, int num, int size,
 }
 
 int main() {
-  const char* str_arr[] = {"gravity", "diamond", "apple",  "horizon", "flame",
+  const char *str_arr[] = {"gravity", "diamond", "apple",  "horizon", "flame",
                            "kernel",  "complex", "jungle", "eclipse", "bridge"};
   int num = (int)(sizeof(str_arr) / sizeof(*str_arr));
   print_str_arr(str_arr, num);
