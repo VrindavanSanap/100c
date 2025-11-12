32fab8213dde523bec4864dd83e7dbfeb3f6c338
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-12 15:56:32 +0530

    add utils.h

---
+++ utils/include/utils/utils.h
@@ -0,0 +1,10 @@
+#ifndef utils_h
+#define utils_h
+
+#include <stdio.h>
+
+void print_int_arr(int arr[], int len);
+void print_float_arr(float arr[], int len);
+void print_str_arr(const char *arr[], int len);
+
+#endif /* utils_h */
