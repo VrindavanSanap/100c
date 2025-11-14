36b2252c38d3d75f93d2764064bff49986a2a22a
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-14 17:50:04 +0530

    saying adios to kiddo stuff

--- basics/foo_bar/bar.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-#include "foo.h"
-
-int main(void) { printf("%d\n", add(2, 3)); }
--- basics/size_int.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("Size of unsigned int: %zu bytes\n", sizeof(unsigned int));
-  printf("Size of int: %zu bytes\n", sizeof(int));
-  return 0;
-}
