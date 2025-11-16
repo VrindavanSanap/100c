ac7e351fd3841fde4ea04b7bc72ff51665c1032b
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-16 06:41:00 +0530

    -

--- advanced_c/comma_operator/comma_operator.c
+++ advanced_c/comma_operator/comma_operator.c
@@ -10,22 +10,5 @@ int main(void) {
   int c = (a, b);
   printf("c = %d\n", c);
 
-  /*
-  The comma operator has the lowest precedence of all C operators.
-  In this case, 'a' is assigned to 'd' because the assignment operator has
-  higher precedence. The expression 'b' is evaluated, but its result is
-  discarded.
-  */
-  int d = a, b;
-  printf("d = %d\n", d);
-
-  /*
-  For example, in the following macro, the return value of assert() is
-  discarded. The macro then returns the value of _Py_CAST(PyListObject *, (op)).
-  #define _PyList_CAST(op) \
-    (assert(PyList_Check(op)), _Py_CAST(PyListObject *, (op)))
-
-  */
-
   return 0;
 }
