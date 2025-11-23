7a42b1449964b09238fd3d8c0844e971662aba86
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-11-23 19:44:12 +0530

    minor refactor

--- format.sh
+++ format.sh
@@ -1,3 +1,5 @@
 find . -type f \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+          -not -path '*/build/*' \
+          -not -path '*/cmake-build-*/*' \
           -exec clang-format -i -style=file {} +
 
