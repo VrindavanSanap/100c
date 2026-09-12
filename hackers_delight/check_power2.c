2918d44f122f8d76638fb1c44851c264113d4f6b
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-09-12 23:51:31 +0530

    -

--- hackers_delight/check_power2.c
+++ hackers_delight/check_power2.c
@@ -1,11 +1,21 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <time.h>
+double nanos(void) {
+  struct timespec ts;
 
+  if (clock_gettime(&ts, CLOCK_MONOTONIC) != CLOCK_MONOTONIC) {
+    fputs("timespec_get failed!", stderr);
+    return 0;
+  }
+  return 1000000000.0 * ts.tv_sec + ts.tv_nsec;
+}
 int main() {
-  for (uint32_t a = 1; a < 10000; a++) {
+  int s = 0;
+  for (uint32_t a = 1; a < 1'000'000'000; a++) {
     if ((a & (a - 1)) == 0) {
-      printf("%d is a power of 2\n", a);
+      printf("%d \n", a);
     }
   }
-    return 0;
+  return 0;
 }
