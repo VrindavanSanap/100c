5d5e550e077a40ed12d6ea01fdf4e325a935444b
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-09-13 17:45:55 +0530

    fixed for macos

--- hackers_delight/check_power2.c
+++ hackers_delight/check_power2.c
@@ -4,18 +4,21 @@
 double nanos(void) {
   struct timespec ts;
 
-  if (clock_gettime(&ts, CLOCK_MONOTONIC) != CLOCK_MONOTONIC) {
+  if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
     fputs("timespec_get failed!", stderr);
     return 0;
   }
   return 1000000000.0 * ts.tv_sec + ts.tv_nsec;
 }
+
 int main() {
-  int s = 0;
-  for (uint32_t a = 1; a < 1'000'000'000; a++) {
+  double st = nanos();
+  for (uint32_t a = 1; a < 4'000'000'000; a++) {
     if ((a & (a - 1)) == 0) {
-      printf("%d \n", a);
+      printf("%u \n", a);
     }
   }
+  double et = nanos();
+  printf("4 billion iterations in %f seconds", (et - st) / 1000000000);
   return 0;
 }
